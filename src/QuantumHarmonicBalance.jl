92a9a677b6ed57eba744a938812c8ae6dbb57964
oameye <orjan.ameye@hotmail.com>
2024-04-02 09:34:44 +0200

    add rotation to exported func

--- src/QuantumHarmonicBalance.jl
+++ src/QuantumHarmonicBalance.jl
@@ -2,5 +2,13 @@ module QuantumHarmonicBalance
 
 using Reexport
 @reexport using QuantumCumulants
+using QuantumOpticsBase
+
+include("rotate.jl")
+export perform_hadamard, rotate
+include("types.jl")
+
+
+
 
 end
